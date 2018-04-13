94861349d74fdb96a09113462ec7e1b0dae1262c
bebenlebricolo <bebenlebricolo@users.noreply.github.com>
2018-04-13 22:46:41 +0200

    Moved to version 0.3 Added simple pulse counter / decimator and recompute pulse period every 250 ms Cleaned project and renamed it. Added .gitignore to subfolder (ignores Debug/ and Release/ folders) Added ATTiny85_Binaries/ folders Added Scripts/ folder Modified FlashAttiny85.bat file Removed Debug and Release folders

--- RPM_converter_v0.2/RPM_converter_v0.2/Debug/Time_related.d
+++
@@ -1,9 +0,0 @@
-Time_related.d Time_related.o: .././Time_related.cpp .././Time_related.h \
- d:\installed\ programs\atmel\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
- d:\installed\ programs\atmel\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h
-
-.././Time_related.h:
-
-d:\installed\ programs\atmel\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
-
-d:\installed\ programs\atmel\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
